43dbcb1418b5cc64ed114e3ebdd22e888db103e9
Archer <Archer_521@foxmail.com>
2017-08-01 20:00:55 +0800

    [U] fix LINE144

--- code/UserInterface/UserInterface/userinterface.cpp
+++ code/UserInterface/UserInterface/userinterface.cpp
@@ -141,7 +141,7 @@ UserInterface::UserInterface (QWidget *parent)
 			ch1 = buffer->dequeue ();
 			ch2 = buffer->dequeue ();
 			R = (ch1 & 0xF8);
-			G = (ch1 << 5) + (ch2 >> 5);
+			G = ((ch1 << 5) | (ch2 >> 3)) & 0xFC;
 			B = (ch2 << 3);
 			paint_area->ans[i][j][0] = GLubyte (R);
 			paint_area->ans[i][j][1] = GLubyte (G);
