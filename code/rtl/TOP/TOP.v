4723cf2112bab916608967784e72582a2966df8a
Archer <Archer_521@foxmail.com>
2017-08-03 00:32:17 +0800

    [U] read frame data (simply tested) 	- 摄像头只可初始化一次，若初始化中断或错误或重新初始化，则需要重新上电。所以注释了OV_INIT.v LINE51。 	- （未测试）目前只处理了QVGA的一帧，即只使用FIFO的一半空间。 	- 目前发给上位机的数据中，前328B为初始化返回值，若初始化成功，则第7字节为0x73，第11字节为0x76，第327字节为0x0D，第328字节为0x0A，其余字节为0x00。 	- （未测试）从第329字节起为图像数据，前320×240×2字节为RGB565数据，最后2字节为0x0D、0x0A。似乎数据仍有错误，需要进一步测试。 	- 目前发送图像数据的逻辑为：当FIFO中写满了一帧QVGA数据，OV_READ模块按字节传给顶层，两字节间隔10us；顶层每接收到一个字节，立即以波特率115200发给上位机。

--- code/rtl/TOP/TOP.v
+++ code/rtl/TOP/TOP.v
@@ -1,7 +1,6 @@
 `timescale 1us / 1us
 
 module TOP (
-    output [15:0] led,
     input sys_clk,
     input rst_n,
     input rx_pin,
@@ -15,7 +14,9 @@ module TOP (
     output rclk, // fifo read clock
     output rrst // fifo read reset
 );
-    /**** Clocks ****/
+    /*-------------------------------------------------
+     |                     Clocks                     |
+     -------------------------------------------------*/
     parameter SYS_CLK_FREQ = 100_000_000;
     parameter BAUD_RATE = 115200;
     
@@ -24,7 +25,9 @@ module TOP (
     CLK_GEN #(SYS_CLK_FREQ, 100_000) clk_gen_100kHz(sys_clk, rst_n, clk_100kHz);
     CLK_GEN #(SYS_CLK_FREQ, 25_000_000) clk_gen_25MHz(sys_clk, rst_n, clk_25MHz);
     
-    /**** UART ****/
+    /*-------------------------------------------------
+     |                      UART                      |
+     -------------------------------------------------*/
     wire rx_error;
     wire rx_finish;
     wire[7:0] rx_data;
@@ -35,25 +38,32 @@ module TOP (
     reg[7:0] tx_data = 8'h00;
     UART_TX tx(clk_uart, rst_n, tx_pin, tx_start, tx_finish, tx_data);
     
-    /**** Camera ****/
+    /*-------------------------------------------------
+     |                     Camera                     |
+     -------------------------------------------------*/
     parameter [7:0] DEVICE_ID = 8'h42;
     
     wire initialized;
-    wire new_data;
-    wire[7:0] data;
-    OV_INIT #(DEVICE_ID) init(clk_100kHz, rst_n, scl, sda, initialized, new_data, data);
+    wire new_init_data;
+    wire[7:0] init_data;
+    OV_INIT #(DEVICE_ID) init(clk_100kHz, rst_n, scl, sda, initialized, new_init_data, init_data);
+        
+    wire new_frame;
+    wire frame_read;
+    wire new_frame_data;
+    wire[7:0] frame_data;
+    OV_STORE ov_s(clk_25MHz, rst_n, initialized, wen, wrst, vsync, new_frame, frame_read);
+    OV_READ ov_r(clk_25MHz, rst_n, initialized, fifo_data, rclk, rrst, new_frame, frame_read, new_frame_data, frame_data);
     
-    // wire new_frame;
-    // wire frame_read;
-    // OV_STORE ov_s(clk_25MHz, rst_n, wen, wrst, vsync, new_frame, frame_read);
-    // OV_READ ov_r(clk_25MHz, rst_n, fifo_data, rclk, rrst, new_frame, frame_read);
-    
-//    reg[0:2] new_data_reg = {3{1'b0}};
-//    always @(posedge sys_clk or negedge rst_n)
-//        if (!rst_n) new_data_reg = {3{1'b0}};
-//        else new_data_reg = {new_data, new_data_reg[0:1]};
-//    always @(posedge sys_clk or negedge rst_n)
-//        if (!rst_n) tx_start <= 1'b0;
-//        else if (tx_start) begin if (!tx_finish) tx_start <= 1'b0; end
-//        else begin tx_start <= new_data_reg[1] & ~new_data_reg[2]; tx_data <= data; end
+    reg[0:2] new_init_data_reg = {3{1'b0}};
+    reg[0:2] new_frame_data_reg = {3{1'b0}};
+    always @(posedge sys_clk or negedge rst_n)
+        if (!rst_n) begin new_init_data_reg = {3{1'b0}}; new_frame_data_reg = {3{1'b0}}; end
+        else if (!initialized) new_init_data_reg = {new_init_data, new_init_data_reg[0:1]};
+        else new_frame_data_reg = {new_frame_data, new_frame_data_reg[0:1]};
+    always @(posedge sys_clk or negedge rst_n)
+        if (!rst_n) tx_start <= 1'b0;
+        else if (tx_start) begin if (!tx_finish) tx_start <= 1'b0; end
+        else if (!initialized) begin tx_start <= new_init_data_reg[1] & ~new_init_data_reg[2]; tx_data <= init_data; end
+        else begin tx_start <= new_frame_data_reg[1] & ~new_frame_data_reg[2]; tx_data <= frame_data; end
 endmodule
