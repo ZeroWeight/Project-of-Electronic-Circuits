51c92f3e346b75c6431360a18b603e208e97bc6b
Zero Weight <zwt15@mails.tsinghua.edu.cn>
2017-08-03 12:39:47 +0800

    [U] output to file, BUG: cant exit normally

--- code/Upper_demo/Upper_demo/main.cpp
+++ code/Upper_demo/Upper_demo/main.cpp
@@ -11,7 +11,7 @@ int main (int argc, char *argv[]) {
 	QCoreApplication a (argc, argv);
 	QSerialPort sp;
 	QTimer timer;
-	timer.setInterval (10);
+	timer.setInterval (15);
 	sp.setPortName ("COM2");
 	sp.setBaudRate (QSerialPort::Baud115200);
 	sp.setFlowControl (QSerialPort::NoFlowControl);
