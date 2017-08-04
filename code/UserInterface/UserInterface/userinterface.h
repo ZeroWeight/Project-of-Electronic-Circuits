180855e01bd9adaf5fe0a378c1a8eae3336f4728
Zero Weight <zwt15@mails.tsinghua.edu.cn>
2017-08-05 07:47:19 +0800

    [U] Temp update with bugs to fix

--- code/UserInterface/UserInterface/userinterface.h
+++ code/UserInterface/UserInterface/userinterface.h
@@ -15,35 +15,34 @@
 #include <QGL>
 #include <QSerialPort>
 #include <QTimer>
+#include <QLabel>
 #include <QSerialPortInfo>
 #include <QMessageBox>
 #include <QFile>
 #include <QTextStream>
 #include <QList>
 #include <QThread>
+#include <QImage>
 #include <QQueue>
-#include <GL/GLU.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 const int buffer_size = 153602;//a image is followed with 0x0D,0x0A
 const int BaudRate = 1382400;
-class Painter :public QGLWidget {
-	Q_OBJECT
-public:
-	int size;
-	GLubyte ans[240][320][3] = { 0 };
-	Painter (QWidget* parent = 0);
-	~Painter ();
-private:
-	void resizeGL (int w, int h)override;
-	void initializeGL ()override;
-	void paintGL () override;
-};
-class Buffer :public QObject, public QQueue<unsigned char> {
+typedef QQueue<unsigned char> * Q;
+class Buffer :public QObject, public QThread {
 	Q_OBJECT
 		signals :
-	void read_out ();
+	void ImageUpdate (const QImage&);
+private:
+	QImage img;
+	QList<Q> list;
+	int index;
+	int fin;
+	unsigned char ans[240][320][3] = { 0 };
+protected:
+	void run ()override;
 public:
+	int _size;
 	void enqueue (const char& t);
 	Buffer (QObject * parent = nullptr);
 };
@@ -92,8 +91,9 @@ public:
 	UserInterface (QWidget *parent = 0);
 	~UserInterface ();
 private:
+	int size;
 	enum class UartState { ON, OFF } currentUartState;
-	Painter* paint_area;
+	QLabel* paint_area;
 	SerialPort *currentSerialPort;
 	Buffer* buffer;
 #ifdef DEBUG
--- code/UserInterface/UserInterface/utility.cpp
+++ code/UserInterface/UserInterface/utility.cpp
@@ -45,13 +45,46 @@ Writer::~Writer () {
 #endif
 
 void Buffer::enqueue (const char& t) {
-	QQueue::enqueue (unsigned char (t));
-	if (this->size () >= (buffer_size << 1)) {
-		emit read_out ();
+	if (list.last ()->size () == buffer_size - 1 && t == 0x0A && list.last ()->at (list.last ()->size () - 2) == 0x0D) {
+		list.last ()->enqueue (t);
+		QQueue<unsigned char>*ptr = new QQueue<unsigned char> ();
+		list.append (ptr);
+		index++;
+		if (!this->isRunning ()) {
+			fin = index - 1;
+			this->start ();
+		}
 	}
+	else if (list.last ()->size () >= 2 && t == 0x0A && list.last ()->at (list.last ()->size () - 2) == 0x0D) {
+		list.last ()->clear ();
+	}
+	else list.last ()->enqueue (t);
 }
 
-Buffer::Buffer (QObject * parent) :QObject (parent) {}
+void Buffer::run () {
+	unsigned char ch1, ch2;
+	for (int i = 0; i < 240; ++i)for (int j = 0; j < 320; ++j) {
+		ch1 = list.at (fin)->dequeue ();
+		ch2 = list.at (fin)->dequeue ();
+		ans[240 - i][j][0] = unsigned char (ch1 & 0xF8);
+		ans[240 - i][j][1] = unsigned char ((ch1 << 5) | (ch2 >> 3)) & 0xFC;
+		ans[240 - i][j][2] = unsigned char (ch2 << 3);
+	}
+	//cv::Mat mat_ori = cv::Mat (240, 320, CV_8UC3, ans);
+	//cv::Mat mat_des = cv::Mat (240 * _size, 320 * _size, CV_8UC3);
+	//cv::Mat rgb;
+	//cvtColor (mat_des, rgb, CV_BGR2RGB);
+	img = QImage ((const unsigned char*)(ans),  //(const unsigned char*)
+		320, 240,
+		240 * 3,
+		QImage::Format_RGB888);
+	emit ImageUpdate (img);
+}
+Buffer::Buffer (QObject * parent) :QObject (parent) {
+	fin = index = 0;
+	QQueue<unsigned char>* ptr = new QQueue<unsigned char> ();
+	list.append (ptr);
+}
 
 SerialPort::SerialPort (QObject* parent) : QObject (parent) {
 	running = false;
