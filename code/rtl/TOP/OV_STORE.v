b2e176c5f6bc354c870ff678d176cdbec0add2d7
Archer <Archer_521@foxmail.com>
2017-08-04 14:25:36 +0800

    [U] ov7670 works well

--- code/rtl/TOP/OV_STORE.v
+++ code/rtl/TOP/OV_STORE.v
@@ -21,18 +21,18 @@ module OV_STORE (
             state <= 3'd0;
         end else if (initialized)
         case (state)
-        3'd0: if (!vsync) state <= 3'd1;
-        3'd1: if (vsync) begin wrst <= 1'b0; state <= 3'd2; end
+        3'd0: if (!vsync) state <= 3'd1; // wait vsync negedge
+        3'd1: if (vsync) begin wrst <= 1'b0; state <= 3'd2; end // wait vsync posedge, write reset
         3'd2: begin wrst <= 1'b1; state <= 3'd3; end
-        3'd3: begin wen <= 1'b1; state <= 3'd4; end
-        3'd4: if (!vsync) state <= 3'd5;
-        3'd5: if (vsync) begin
-            wen <= 1'b0;
-            if (frame_cnt == 4'd10) noisy <= 1'b0;
+        3'd3: begin wen <= 1'b1; state <= 3'd4; end // write enable
+        3'd4: if (!vsync) state <= 3'd5; // wait vsync negedge, frame written
+        3'd5: if (vsync) begin // wait vsync posedge
+            wen <= 1'b0; // write disable
+            if (frame_cnt == 4'd10) noisy <= 1'b0; // 不管前10帧稳不稳定反正老子不要了
             if (noisy) begin frame_cnt <= frame_cnt + 1'd1; state <= 3'd0; end
             else begin new_frame <= 1'b1; state <= 3'd6; end
         end
-        3'd6: if (!frame_read) state <= 3'd7;
+        3'd6: if (!frame_read) state <= 3'd7; // wait read begin
         3'd7: if (frame_read) begin new_frame <= 1'b0; state <= 3'd0; end
         endcase
 endmodule
