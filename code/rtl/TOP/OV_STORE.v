a17ecaa5624492b8dad250ea3e4e49dc81a88e88
Archer <Archer_521@foxmail.com>
2017-08-04 12:30:52 +0800

    [U] ov7670 generally works :bangbang: 	:large_blue_circle: 修改了一下初始化配置，不一定比原来好，可以进一步测试。现在初始化返回值共517字节。 	:large_blue_circle: 初始化后的几帧可能不太稳定，直接舍弃。 	:large_blue_circle: 读取图像速度改为100us每字节，需在上位机方面优化。 	:red_circle: 图像可能出现某块状或条状区域数据异常，表现为彩块或彩条。

--- code/rtl/TOP/OV_STORE.v
+++ code/rtl/TOP/OV_STORE.v
@@ -4,33 +4,35 @@ module OV_STORE (
     input clk_25MHz,
     input rst_n,
     input initialized,
-    output reg wen = 1'b1,
+    output reg wen = 1'b0,
     output reg wrst = 1'b1,
     input vsync,
     output reg new_frame = 1'b0,
     input frame_read
-);
-    reg[0:2] vsync_reg = {3{1'b0}};
-    always @(posedge clk_25MHz or negedge rst_n)
-        if (!rst_n) vsync_reg <= {3{1'b0}};
-        else if (initialized) vsync_reg <= {vsync, vsync_reg[0:1]};
-    wire vsync_irq = vsync_reg[1] & ~vsync_reg[2]; // posedge
-    
+);    
+    reg[3:0] frame_cnt = 1'd0;
+    reg noisy = 1'b1;
     reg[2:0] state = 3'd0;
     always @(posedge clk_25MHz or negedge rst_n)
         if (!rst_n) begin
-            wen <= 1'b0; wrst <= 1'b0;
+            wen <= 1'b0; wrst <= 1'b1;
             new_frame <= 1'b0;
+            frame_cnt <= 1'd0; noisy <= 1'b1;
             state <= 3'd0;
         end else if (initialized)
         case (state)
-        3'd0: if (vsync_irq) state <= 3'd1;
-        3'd1: begin wrst <= 1'b0; state <= 3'd2; end
-        3'd2: begin wen <= 1'b1; wrst <= 1'b1; state <= 3'd3; end
-        3'd3: if (vsync_irq) state <= 3'd4;
-        3'd4: begin wen <= 1'b0; new_frame <= 1'b1; state <= 3'd5; end
-        3'd5: if (!frame_read) state <= 3'd6;
-        3'd6: if (frame_read) begin new_frame <= 1'b0; state <= 3'd0; end
-        default: state <= 3'd0;
+        3'd0: if (!vsync) state <= 3'd1;
+        3'd1: if (vsync) begin wrst <= 1'b0; state <= 3'd2; end
+        3'd2: begin wrst <= 1'b1; state <= 3'd3; end
+        3'd3: begin wen <= 1'b1; state <= 3'd4; end
+        3'd4: if (!vsync) state <= 3'd5;
+        3'd5: if (vsync) begin
+            wen <= 1'b0;
+            if (frame_cnt == 4'd10) noisy <= 1'b0;
+            if (noisy) begin frame_cnt <= frame_cnt + 1'd1; state <= 3'd0; end
+            else begin new_frame <= 1'b1; state <= 3'd6; end
+        end
+        3'd6: if (!frame_read) state <= 3'd7;
+        3'd7: if (frame_read) begin new_frame <= 1'b0; state <= 3'd0; end
         endcase
 endmodule
